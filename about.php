6066e534e2497f0ac6ddb7b5584360af8c46e1ea
Simeon Offiong <simeonoffiong@gmail.com>
2020-08-18 19:41:05 -0600

    adding about page to the repository

---
+++ about.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>About - Simicodes</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <style>
+    /*body{*/
+        /*background: #e2e2e2;*/
+        /*background: #EEEEEE;*/
+    /*}*/
+    
+    .profile {
+        /*background: #e2e2e2;*/
+        margin-bottom: 20px;
+    }
+    
+    .profile-top {
+        min-height: 200px;
+        /*background: #999;*/
+        background: #337AB7;
+    }
+    
+    .profile-image {
+        margin-top: -100px;
+        height: 200px;
+        width: 200px;
+        border-radius: 100px;
+        border: 8px solid #eee;
+    }
+    
+    .profile-body {
+        padding: 20px;
+        text-align: center;
+        color: #666;
+    }
+    </style>
+
+    <body>
+        <div class="profile">
+            <div class="profile-top"></div>
+            <div class="text-center">
+                <img src="images/hipster.jpg" class="profile-image">
+            </div>
+            <div class="profile-body">
+                <h3>Simeon Offiong
+                                <br>
+                                <small>Intern Software Engineer</small>
+                            </h3>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-sm-12">
+                <div class="panel panel-default" style="background: #eee;">
+                    <div class="jumbotron text-center">
+                        <h1 class="display-3">About Me</h1>
+                        <p class="lead">I am a Web Developer/Programmer. passionate about solving problems and providing solutions. I currently focused on building responsive user interfaces and maintaining the front-end functionalities for web application. Hopefully to build stuff for Web/Android platforms. </p>
+                        <hr class="m-y-md">
+                        <p>I am a Computer Science student in the University of Calabar, Nigeria. interested in Software Engineering.</p>
+                        <p class="lead">
+                            <a class="btn btn-primary btn-lg" href="index.php" role="button">Home</a>
+                            <a class="btn btn-primary btn-lg" href="contact.php" role="button">Contact</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
